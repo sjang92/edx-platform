c8251f879613f75a3bbf2a71199e3c77825d851a
Muhammad Ammar <mammar@gmail.com>
2014-07-02 09:53:52 +0000

    handouts

--- common/test/acceptance/tests/video/test_video_handout.py
+++ common/test/acceptance/tests/video/test_video_handout.py
@@ -4,12 +4,102 @@
 Acceptance tests for CMS Video Handout.
 """
 
+from unittest import skipIf
+from ...pages.studio.auto_auth import AutoAuthPage
+from ...pages.studio.overview import CourseOutlinePage
+from ...fixtures.course import CourseFixture, XBlockFixtureDesc
+from ..helpers import UniqueCourseTest, is_youtube_available
 
-class VideoHandoutBaseTest(object):
-    pass
+
+@skipIf(is_youtube_available() is False, 'YouTube is not available!')
+class VideoHandoutBaseTest(UniqueCourseTest):
+
+    def setUp(self):
+        """
+        Initialization of pages and course fixture for tests
+        """
+        super(VideoHandoutBaseTest, self).setUp()
+
+        self.outline = CourseOutlinePage(
+            self.browser,
+            self.course_info['org'],
+            self.course_info['number'],
+            self.course_info['run']
+        )
+
+        self.course_fixture = CourseFixture(
+            self.course_info['org'], self.course_info['number'],
+            self.course_info['run'], self.course_info['display_name']
+        )
+
+        self.handout = None
+
+    def navigate_to_unit(self):
+        """
+        Install the course with required components and navigate to course unit page
+        """
+        self._install_course_fixture()
+        self._navigate_to_course_unit_page()
+
+    def _install_course_fixture(self):
+        """
+        Prepare for tests by creating a course with a section, subsection, and unit.
+        Performs the following:
+            Create a course with a section, subsection, and unit
+            Create a user and make that user a course author
+            Log the user into studio
+        """
+        video_metadata = None
+
+        if self.handout:
+            self.course_fixture.add_asset([self.handout])
+            video_metadata = {'handout': '/static/{handout}'.format(handout=self.handout)}
+
+        # Create course with Video component
+        self.course_fixture.add_children(
+            XBlockFixtureDesc('chapter', 'Test Section').add_children(
+                XBlockFixtureDesc('sequential', 'Test Subsection').add_children(
+                    XBlockFixtureDesc("vertical", "Test Unit").add_children(
+                        XBlockFixtureDesc('video', 'Video', metadata=video_metadata),
+                    )
+                )
+            )
+        ).install()
+
+        # Auto login and register the course
+        self.auth_page = AutoAuthPage(
+            self.browser,
+            staff=False,
+            username=self.course_fixture.user.get('username'),
+            email=self.course_fixture.user.get('email'),
+            password=self.course_fixture.user.get('password')
+        ).visit()
+
+    def _navigate_to_course_unit_page(self):
+        """
+        Open the course from the dashboard and expand the section and subsection and click on the Unit link
+        The end result is the page where the user is editing the newly created unit
+        """
+        # Visit Course Outline page
+        self.outline.visit()
+
+        # Visit Unit page
+        self.outline.section('Test Section').subsection('Test Subsection').toggle_expand().unit('Test Unit').go_to()
+
+        # TODO! Add wait to ensure that all the required components have been loaded before doing any further action
 
 
 class VideoHandoutTest(VideoHandoutBaseTest):
 
-    def test_handouts_upload_correctly(self):
-        pass
+    def test_handout_uploads_correctly(self):
+        """
+        Scenario: Handout uploading works correctly
+        Given I have created a Video component with handout file "textbook.pdf"
+        And I can download handout file with mime type "application/pdf"
+        """
+        self.handout = 'textbook.pdf'
+
+        self.navigate_to_unit()
+
+        from nose.tools import  set_trace; set_trace()
+
