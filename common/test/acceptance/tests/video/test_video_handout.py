bcce5edf92f38a9312acd5bdd6cfbf034afffdee
Muhammad Ammar <mammar@gmail.com>
2014-07-02 06:00:44 +0000

    Initial test file for video handout

---
+++ common/test/acceptance/tests/video/test_video_handout.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+"""
+Acceptance tests for CMS Video Handout.
+"""
+
+
+class VideoHandoutBaseTest(object):
+    pass
+
+
+class VideoHandoutTest(VideoHandoutBaseTest):
+
+    def test_handouts_upload_correctly(self):
+        pass
